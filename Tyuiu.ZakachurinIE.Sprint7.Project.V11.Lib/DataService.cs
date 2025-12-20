f2dd84ff7d63147e69ebd81e89ea3e0a37ec8b3f
Иван Закачурин <vanekcarek43@gmail.com>
2025-12-20 17:08:19 +0500

    AnotherMethodsWithTests

--- Tyuiu.ZakachurinIE.Sprint7.Project.V11.Lib/DataService.cs
+++ Tyuiu.ZakachurinIE.Sprint7.Project.V11.Lib/DataService.cs
@@ -20,48 +20,92 @@ namespace Tyuiu.ZakachurinIE.Sprint7.Project.V11.Lib
         }
 
         
-        private List<Employee> _employees;
+        private List<Employee> employees;
 
         public DataService()
         {
-            _employees = new List<Employee>(); // делаю списочек
+            employees = new List<Employee>(); // делаю списочек
         }
 
         public void LoadFromFile(string filePath) // этот метод читает данные из файла.ксв и заполняет колоночки в списке
         {            
-            _employees.Clear();          
+            employees.Clear();          
             string[] lines = File.ReadAllLines(filePath);           
             for (int i = 0; i < lines.Length; i++)
             {
                 string[] columns = lines[i].Split(';');
                 Employee sotrudnik = new Employee
                 {Surname = columns[0],Name = columns[1],Patronymic = columns[2],Address = columns[3],PhoneNumber = columns[4],BirthDate = DateTime.Parse(columns[5]),Position = columns[6],StartDate = DateTime.Parse(columns[7]),ExperienceYears = int.Parse(columns[8])};
-                _employees.Add(sotrudnik);
+                employees.Add(sotrudnik);
             }
 
         }
         public List<Employee> GetAllEmployees() // выводим весь список всех сотрудников
         {
-            return _employees;
+            return employees;
         }
         public List<Employee> SearchBySurname(string surname) // следуя из названия - поиск по фамилии, для которого не обязательна полная фамилия
         {
-            return _employees.Where(e => e.Surname.IndexOf(surname, StringComparison.OrdinalIgnoreCase)>=0).ToList();
+            return employees.Where(e => e.Surname.IndexOf(surname, StringComparison.OrdinalIgnoreCase)>=0).ToList();
         }       
         public List<Employee> SearchByName(string name) // поиск по имени, можно написать лишь часть имени
         {
-            return _employees.Where(e => e.Name.IndexOf(name, StringComparison.OrdinalIgnoreCase)>=0).ToList();
+            return employees.Where(e => e.Name.IndexOf(name, StringComparison.OrdinalIgnoreCase)>=0).ToList();
         }        
         public List<Employee> SearchByPatronymic(string patronymic) // поиск по отчеству
         {
-            return _employees.Where(e => e.Patronymic.IndexOf(patronymic, StringComparison.OrdinalIgnoreCase)>=0).ToList();
-
+            return employees.Where(e => e.Patronymic.IndexOf(patronymic, StringComparison.OrdinalIgnoreCase)>=0).ToList();
         }
         public List<Employee> SearchByPhone(string phone) // поиск по телефону, также можно написать только часть номера, а не весь
         {
-            return _employees.Where(e => e.PhoneNumber.Contains(phone.Trim())).ToList();
+            return employees.Where(e => e.PhoneNumber.Contains(phone.Trim())).ToList();
         }
-
-
+        public List<Employee> SearchByExperience(int minYears, int maxYears) // поиск по стажу работы, где можно выставить от и до 
+        {
+            return employees.Where(e => e.ExperienceYears >= minYears && e.ExperienceYears <= maxYears).ToList();
+        }
+        public List<Employee> SortByExperience() // сортировка стажа по возрастанию
+        {
+            return  employees.OrderBy(e => e.ExperienceYears).ToList();
+        }
+        public List<Employee> SortByBirthDate() // сортировка по дате рождения по возрастанию
+        {
+            return employees.OrderBy(e => e.BirthDate).ToList();
+        }
+        public List<Employee> SortByStartDate() // сортировка по вступлению на работу по возрастанию
+        {
+            return employees.OrderBy(e => e.StartDate).ToList();
+        }
+        public List<Employee> SortByExperienceBack() // сортировка по стажу на убывание
+        {
+            return employees.OrderByDescending(e => e.ExperienceYears).ToList();
+        }
+        public List<Employee> SortByBirthDateBack() // сортировка по дате рождения по убыванию
+        {
+            return employees.OrderByDescending(e => e.BirthDate).ToList();
+        }
+        public List<Employee> SortByStartDateBack() // сортировка по вступлению на работу по убыванию
+        {
+            return employees.OrderByDescending(e => e.StartDate).ToList();
+        }
+        public int GetEmployeeCount() // количество сотрудников
+        {
+            return employees.Count;
+        }
+        public double GetAverageExperience() // средний стаж сотрудника
+        {
+            if (employees.Count == 0) return 0;
+            return employees.Average(e => e.ExperienceYears);
+        }
+        public int GetMinExperience() // минимальный стаж
+        {
+            if (employees.Count == 0) return 0;
+            return employees.Min(e => e.ExperienceYears);
+        }
+        public int GetMaxExperience() // максимальный стаж
+        {
+            if (employees.Count == 0) return 0;
+            return employees.Max(e => e.ExperienceYears);
+        }
     }
 }
--- Tyuiu.ZakachurinIE.Sprint7.Project.V11.Test/DataServiceTest.cs
+++ Tyuiu.ZakachurinIE.Sprint7.Project.V11.Test/DataServiceTest.cs
@@ -60,5 +60,89 @@ namespace Tyuiu.ZakachurinIE.Sprint7.Project.V11.Test
             Assert.IsTrue(result.Count > 0);
             Assert.IsTrue(result.All(e => e.PhoneNumber.Contains("8999")));
         }
+        [TestMethod]
+        public void SearchByExperienceRange_ValidRange_ReturnsCorrectEmployees()
+        {
+           
+            DataService ds = new DataService();
+            string path = $@"C:\DataSprint7\Employees.csv";
+            ds.LoadFromFile(path);
+            var result = ds.SearchByExperience(5, 12);            
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual(10, result[0].ExperienceYears);
+        }
+        [TestMethod]
+        public void SortByExperienceValid()
+        {
+            DataService ds = new DataService();
+            string path = $@"C:\DataSprint7\Employees.csv";
+            ds.LoadFromFile(path);
+            var sorted = ds.SortByExperience();
+            for (int i = 0; i < sorted.Count - 1; i++)
+            {
+                Assert.IsTrue(sorted[i].ExperienceYears <= sorted[i + 1].ExperienceYears);
+            }
+        }
+        [TestMethod]
+        public void SortByExperienceBackValid()
+        {
+            DataService ds = new DataService();
+            string path = $@"C:\DataSprint7\Employees.csv";
+            ds.LoadFromFile(path);
+            var sorted = ds.SortByExperienceBack();
+            for (int i = 0; i < sorted.Count - 1; i++)
+            {
+                Assert.IsTrue(sorted[i].ExperienceYears >= sorted[i + 1].ExperienceYears);
+            }
+        }
+        [TestMethod]
+        public void SortByBirthDateValid()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\DataSprint7\Employees.csv";
+            ds.LoadFromFile(path);
+            var sorted = ds.SortByBirthDate();
+            for (int i = 0; i < sorted.Count - 1; i++)
+            {
+                Assert.IsTrue(sorted[i].BirthDate <= sorted[i + 1].BirthDate);
+            }
+        }
+        [TestMethod]
+        public void SortByBirthDateBackValid()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\DataSprint7\Employees.csv";
+            ds.LoadFromFile(path);
+            var sorted = ds.SortByBirthDateBack();
+            for (int i = 0; i < sorted.Count - 1; i++)
+            {
+                Assert.IsTrue(sorted[i].BirthDate >= sorted[i + 1].BirthDate);
+            }
+        }
+        [TestMethod]
+        public void SortByStartDateValid()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\DataSprint7\Employees.csv";
+            ds.LoadFromFile(path);
+            var sorted = ds.SortByStartDate();
+            for (int i = 0; i < sorted.Count - 1; i++)
+            {
+                Assert.IsTrue(sorted[i].StartDate <= sorted[i + 1].StartDate);
+            }
+        }
+        [TestMethod]
+        public void SortByStartDateBackValid()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\DataSprint7\Employees.csv";
+            ds.LoadFromFile(path);
+            var sorted = ds.SortByStartDateBack();
+            for (int i = 0; i < sorted.Count - 1; i++)
+            {
+                Assert.IsTrue(sorted[i].StartDate >= sorted[i + 1].StartDate);
+            }
+        }
+
     }
 }
